f0394af633b39923cb6b9cf38c65125f17ac5b6a
Mudassar1998 <khanqureshi31181@gmail.com>
2022-11-09 10:21:37 -0600

    set the routes again

---
+++ src/components/Athletic.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Athletic = () => {
+  return (
+    <div>
+      <main> Athletic Page</main>
+    </div>
+  )
+}
+
+export default Athletic
---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div>
+      <main> Cart Page</main>
+    </div>
+  )
+}
+
+export default Cart
