e159ed1ba0dd8a9ef92994efed61ac144ab4c540
stevezhao <steve.zhao@gmail.com>
2022-11-09 15:33:56 -0500

    order-shoe model update

--- server/db/order-shoe.js
+++ server/db/order-shoe.js
@@ -2,6 +2,13 @@ const Sequelize = require("sequelize")
 const db = require('./database')
 
 const order_shoe = db.define("order_shoe", {
+    orderId: {
+        type: Sequelize.INTEGER,
+
+    },
+    productId: {
+        type: Sequelize.INTEGER,
+    },
     unitPrice: {
         type: Sequelize.INTEGER,
         defaultValue: 0,
