7a4e8a54486834c810942fbe24dc6bce5e4e17b5
josephdabit <josephdabit@gmail.com>
2022-11-09 09:53:25 -0700

    User get/put routes added

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 router.use("/shoes", require("./shoes"))
 router.use("/orders", require("./orders"))
+router.use("/users", require("./users"))
 
 
 router.use((req, res, next) => {
---
+++ server/api/users.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const { user } = require('../db')
+
+// GET for /api/users/:id
+router.get('/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params
+        res.send(await user.findByPk(id))
+    } catch (err) {
+        next(err)
+    }
+})
+
+// PUT for /api/users/:id
+router.put('/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const updateAccount = await user.findByPk(id);
+        res.send(await updateAccount.update(req.body));
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
