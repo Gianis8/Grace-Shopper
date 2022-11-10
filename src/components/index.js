de9e4b6ef967ce22ee0fb765caec6c07f4ea0d55
Mudassar1998 <khanqureshi31181@gmail.com>
2022-11-10 09:37:07 -0600

    change Main and index.js in components

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
 export {default as Main} from './Main.js'
 export {default as HomePage} from './HomePage'
 export {default as Athletic} from './Athletic'
 export {default as Casual} from './Casual'
 export {default as Cart} from './Cart'
 export {default as Login} from './Login'
-
-=======
-export {default as Main} from './Main.js'
-export {default as HomePage} from './HomePage'
-export {default as Athletic} from './Athletic'
-export {default as Casual} from './Casual'
-export {default as Cart} from './Cart'
-export {default as Login} from './Login'
->>>>>>> main
