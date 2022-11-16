0e90d9bf34ed1159959a0e38cc48dd4a02200d8b
Mudassar1998 <khanqureshi31181@gmail.com>
2022-11-16 15:43:46 -0600

    made sign up changes

--- src/components/AdminOrders.js
+++ src/components/AdminOrders.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { authenticate } from '../store/authSlice'
 import { fetchCartAsync, selectCart } from '../store/ordersSlice';
@@ -9,7 +9,7 @@ import { fetchCartAsync, selectCart } from '../store/ordersSlice';
 **/
 
 const AdminOrders = () => {
- 
+
   const dispatch = useDispatch()
   const orders = useSelector(selectCart)
   console.log(orders)
@@ -17,38 +17,20 @@ const AdminOrders = () => {
     dispatch(fetchCartAsync())
   }, [dispatch])
 
-  async function handleSubmit(evt) {
-    evt.preventDefault()
 
-  }
 
-  return (
-    <>
+return (
+  <>
+    <section>
       <h1> User Orders</h1>
-<section> 
+
       <ul>{orders.map((order) => {
-        return <li key={order.id}><h3>{order.cost}</h3> <p>{order.shippingAddress}</p></li>
+        return <li key={order.id}><h3>{order.cost}</h3> <h4>{order.shippingAddress}</h4></li>
       })}</ul>
-</section>
- {/* <div> */}
-      {/* <form onSubmit={handleSubmit}>
-        <h3>UserName</h3>
-        <label htmlFor='order1'>Order1</label>
-        <input type ='number' value={order} onChange={(evt) => setOrder(+evt.target.value)} />
-        <label htmlFor='order2'>Order2</label>
-        <input type ='number' value={order} onChange={(evt) => setOrder(+evt.target.value)} />
-        <label htmlFor='order3'>Order3</label>
-        <input type ='number' value={order} onChange={(evt) => setOrder(+evt.target.value)} />
-        <label htmlFor='order4'>Order4</label>
-        <input type ='number' value={order} onChange={(evt) => setOrder(+evt.target.value)} />
-        <label htmlFor='order5'>Order5</label>
-        <input type ='number' value={order} onChange={(evt) => setOrder(+evt.target.value)} />
-      </form> */}
-     
-      {/* </div> */}
-      
-    </>
-  )
+    </section>
+
+  </>
+)
 }
 
 export default AdminOrders
